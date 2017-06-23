b4502e3f6fa90801ad6010e2025445b973cc2383
dlondon12 <dlondon12@gmail.com>
2017-06-23 12:52:56 -0400

    Update filter_rawdata.m
    Added separate filtering for the local LFP (meaning the broadband signal with spikes filtered out).

--- filter_rawdata.m
+++ filter_rawdata.m
@@ -1,8 +1,18 @@
 function ao=filter_rawdata(ao)
 fs=ao.fs;
-bpFilt = designfilt('bandpassfir','FilterOrder',1000, ...
+ao.fs_low=ao.fs/44;
+bpFilt = designfilt('bandpassfir','FilterOrder',500, ...
     'CutoffFrequency1',250,'CutoffFrequency2',10000, ...
     'SampleRate',fs);
+lfpFilt = designfilt('bandpassfir','FilterOrder',500, ... %% for LFP
+    'CutoffFrequency1',0.1,'CutoffFrequency2',50, ...
+    'SampleRate',ao.fs_low);
+
+%lpFilt = designfilt('bandstopfir','FilterOrder',500, ... %% use to get rid of slow oscillations
+%    'CutoffFrequency1',0.5,'CutoffFrequency2',3, ...
+%    'SampleRate',ao.fs_low);
+
 ao.bp=filtfilt(bpFilt,ao.dat);
-ao.lfp=resample(ao.dat,1,46);%switch this to a median filter?
-ao.fs_low=ao.fs/44;
+ao.dat=resample(ao.dat,1,44); %% for syncing to LFP
+ao.lfp=filtfilt(lfpFilt,ao.dat);
+%ao.lfp=filtfilt(lpFilt,ao.lfp);  %% use to get rid of slow oscillations
